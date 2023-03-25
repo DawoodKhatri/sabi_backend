1ae30a868ba5ebe642b59ca1b8e952540faea8d6
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-03-25 14:47:04 +0530

    odded new origin

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
-  cors({ origin: ["http://localhost:3000", "https://sabi-dine.vercel.app"] })
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://sabi-dine.vercel.app",
+      "https://sabi-test.onrender.com",
+    ],
+  })
 );
 
 require("dotenv").config({ path: "./config/config.env" });
