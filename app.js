5f038b033194db594c20bf2b91e6094ee561c198
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-03-25 13:55:18 +0530

    added 404 route

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({origin: ["http://localhost:3000", "https://sabi-test.onrender.com"]}));
+app.use(
+  cors({ origin: ["http://localhost:3000", "https://sabi-test.onrender.com"] })
+);
 
 require("dotenv").config({ path: "./config/config.env" });
 
@@ -19,5 +21,12 @@ app.use("/api", user);
 app.use("/api", restaurant);
 app.use("/api", product);
 app.use("/api", table);
+
+app.use("*", (req, res) => {
+  return res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
 
 module.exports = app;
